d0a9994b19d03de5535fbb07a116633935a0593f
Huseyin <seyitayaga6363@gmail.com>
2025-05-24 12:22:11 +0300

    first commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Hüseyin Ayağa on 24.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
